1c1b4a6c63869bcc120bae5cf192203939c53390
MiekeH <63008309+MiekeH@users.noreply.github.com>
2020-11-26 16:41:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Magic Mystic Monkeys Website";
+   
+}
+
+function myFunction1() {
+  document.getElementById("demo1").innerHTML = "Magic Mystic Monkeys";
+  
+  
+}
+
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = Date();
+  
+}
+
+function myFunction3() {
+  document.getElementById("demo3").innerHTML = "Hello This is My Picture underneath the Jacket,...";
+}
+
+function myFunction4() {
+  document.getElementById("demo4").innerHTML = "List of three key Items";
+  }
+
+function myFunction5() {
+document.getElementById("demo5").style-backgroundColor = "yellow";
+  
+}
+
+function myFunction6() {
+  document.getElementById("demo6").innerHTML = "Hello interested in my Career path: Please find herewith a list of my work experiences";
+ }
